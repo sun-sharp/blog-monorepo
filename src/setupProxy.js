80350cf399d66a2f6271777e7caf3c25c48110a1
yangruirui <ncmuhengyue@163.com>
2021-10-09 11:52:15 +0800

    修改环境问题

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,9 @@ const proxy = require('http-proxy-middleware');
 let target = '';
 if ( process.env.BABEL_ENV === "development") {
   target = "http://localhost:3000";
+  // target = "http://120.79.162.189:1111";
 } else {
-  target = "http://192.144.238.170:1111";
+  target = "http://120.79.162.189:1111";
 }
 console.log(process.env.BABEL_ENV, "运行环境", target);
 module.exports = function(app) {
