070978b2fa0a36714b5e30f167f6d14e4b294986
yangruirui <ncmuhengyue@163.com>
2021-10-24 19:17:46 +0800

    修改测试环境

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,9 +3,8 @@ const proxy = require('http-proxy-middleware');
 let target = '';
 if ( process.env.BABEL_ENV === "development") {
   target = "http://localhost:3000";
-  // target = "http://120.79.162.189:1111";
 } else {
-  target = "http://120.79.162.189:1111";
+  target = "https://www.yangruirui.top";
 }
 console.log(process.env.BABEL_ENV, "运行环境", target);
 module.exports = function(app) {
