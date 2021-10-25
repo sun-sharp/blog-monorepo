d002d7e87aed7b0292df5efec6dbac972c677d49
yangruirui <ncmuhengyue@163.com>
2021-10-25 17:50:01 +0800

    添加eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// vite 配置
 export default defineConfig({
   plugins: [vue()],
   resolve: {
