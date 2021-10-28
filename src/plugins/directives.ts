b1f9f70be2ee3f1a93074b5aa9cc533a842ebf33
yangruirui <ncmuhengyue@163.com>
2021-10-28 23:43:15 +0800

    修改bug

--- src/plugins/directives.ts
+++ src/plugins/directives.ts
@@ -1,7 +1,7 @@
 // import { App } from 'vue';
 
-// /**
-//  * 注册全局自定义指令
-//  * @param app
-//  */
-// export function setupDirectives(app: App) {}
+/**
+ * 注册全局自定义指令
+ * @param app
+ */
+export function setupDirectives() {}
