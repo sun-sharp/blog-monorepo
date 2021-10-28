fd17fb20bc91f6ca2d233e0fa5ca57eaab3ee20c
yangruirui <ncmuhengyue@163.com>
2021-10-28 21:50:51 +0800

    加上路由变化

--- mock/user/menus.ts
+++ mock/user/menus.ts
@@ -10,7 +10,7 @@ const menusList = [
     redirect: '/home/index',
     meta: {
       // sort: 10,
-      isRoot: true,
+      // isRoot: true,
       icon: 'HomeOutlined',
       // activeMenu: 'home_index',
     },
@@ -24,6 +24,85 @@ const menusList = [
         },
       },
     ],
+  },
+  {
+    path: '/result',
+    name: 'Result',
+    redirect: '/result/success',
+    component: 'LAYOUT',
+    meta: {
+      title: '结果页面',
+      icon: 'CheckCircleOutlined',
+      sort: 4,
+    },
+    children: [
+      {
+        path: 'success',
+        name: 'result-success',
+        meta: {
+          title: '成功页',
+          icon: 'CheckCircleOutlined',
+        },
+        component: '/result/success',
+      },
+      {
+        path: 'fail',
+        name: 'result-fail',
+        meta: {
+          title: '失败页',
+          icon: 'CheckCircleOutlined',
+        },
+        component: '/result/fail',
+      },
+      {
+        path: 'info',
+        name: 'result-info',
+        meta: {
+          title: '信息页',
+          icon: 'CheckCircleOutlined',
+        },
+        component: '/result/info',
+      },
+    ],
+  },
+  {
+    path: '/frame',
+    name: 'Frame',
+    redirect: '/frame/docs',
+    component: 'LAYOUT',
+    meta: {
+      title: '外部页面',
+      sort: 8,
+      icon: 'DesktopOutline',
+    },
+    children: [
+      {
+        path: 'docs',
+        name: 'frame-docs',
+        meta: {
+          title: '项目文档(内嵌)',
+          frameSrc: 'https://naive-ui-admin-docs.vercel.app',
+        },
+      },
+      {
+        path: 'naive',
+        name: 'frame-naive',
+        meta: {
+          title: 'NaiveUi(内嵌)',
+          frameSrc: 'https://www.naiveui.com',
+        },
+      },
+    ],
+  },
+  {
+    path: '/external',
+    name: 'https://naive-ui-admin-docs.vercel.app',
+    component: 'LAYOUT',
+    meta: {
+      title: '项目文档',
+      icon: 'DocumentTextOutline',
+      sort: 9,
+    },
   },
 ];
 
--- src/router/router-icons.ts
+++ src/router/router-icons.ts
@@ -1,9 +1,12 @@
 import { renderIcon } from '@/utils/index';
-import { DashboardOutlined, HomeOutlined } from '@vicons/antd';
-// import { HomeOutline } from '@vicons/ionicons5';
+import { DashboardOutlined, HomeOutlined, CheckCircleOutlined } from '@vicons/antd';
+import { DesktopOutline, DocumentTextOutline } from '@vicons/ionicons5';
 
 //前端路由图标映射表
 export const constantRouterIcon = {
   DashboardOutlined: renderIcon(DashboardOutlined),
   HomeOutlined: renderIcon(HomeOutlined),
+  CheckCircleOutlined: renderIcon(CheckCircleOutlined),
+  DesktopOutline: renderIcon(DesktopOutline),
+  DocumentTextOutline: renderIcon(DocumentTextOutline),
 };
