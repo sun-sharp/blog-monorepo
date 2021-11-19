9716bc214dd1fbab914a364683badd10275c3dd8
powerfulYangRR <ncmuhengyue@163.com>
2021-11-19 21:40:19 +0800

    集成 swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 加入swagger
+  const config = new DocumentBuilder()
+    .setTitle('NestJs博客API')
+    .setDescription('我的测试博客API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
+  console.log('http://localhost:3000');
 }
 bootstrap();
