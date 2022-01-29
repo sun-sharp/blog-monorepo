c332087559a704afce7834ddd5c6027b7dadf4a7
yangruirui <ncmuhengyue@163.com>
2022-01-29 17:26:41 +0800

    修改打包环境

--- build/vite/proxy.ts
+++ build/vite/proxy.ts
@@ -20,6 +20,8 @@ export function createProxy(list: ProxyList = []) {
   for (const [prefix, target] of list) {
     const isHttps = httpsRE.test(target);
 
+    console.log(prefix, target);
+
     // https://github.com/http-party/node-http-proxy#options
     ret[prefix] = {
       target: target,
@@ -30,5 +32,6 @@ export function createProxy(list: ProxyList = []) {
       ...(isHttps ? { secure: false } : {}),
     };
   }
+  console.log(ret);
   return ret;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import type { UserConfig, ConfigEnv } from 'vite';
 import { loadEnv } from 'vite';
 import { resolve } from 'path';
-import { wrapperEnv } from './build/utils';
-import { createVitePlugins } from './build/vite/plugin';
-import { OUTPUT_DIR } from './build/constant';
-import { createProxy } from './build/vite/proxy';
 import pkg from './package.json';
 import { format } from 'date-fns';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import html from 'vite-plugin-html';
+import { createProxy } from './build/vite/proxy';
+// import { viteMockServe } from 'vite-plugin-mock';
 
 const { dependencies, devDependencies, name, version } = pkg;
 const __APP_INFO__ = {
@@ -14,34 +15,103 @@ const __APP_INFO__ = {
   lastBuildTime: format(new Date(), 'yyyy-MM-dd HH:mm:ss'),
 };
 
+// 处理replacement链接
 function pathResolve(dir: string) {
-  return resolve(process.cwd(), '.', dir);
+  return resolve(__dirname, dir);
 }
+
+/**
+ * @description: 处理env文件
+ * @param {Recordable} envConf
+ * @return {ViteEnv}
+ */
+// 将所有环境变量配置文件读取到 process.env
+export function wrapperEnv(envConf: Recordable): ViteEnv {
+  const ret: any = {};
+
+  for (const envName of Object.keys(envConf)) {
+    let realName = envConf[envName].replace(/\\n/g, '\n');
+    realName = realName === 'true' ? true : realName === 'false' ? false : realName;
+
+    if (envName === 'VITE_PORT') {
+      realName = Number(realName);
+    }
+    if (envName === 'VITE_PROXY') {
+      try {
+        realName = JSON.parse(realName);
+      } catch (error) {}
+    }
+    ret[envName] = realName;
+    process.env[envName] = realName;
+  }
+  return ret;
+}
+
+/**
+ * @description: 处理代理数据
+ */
+// function createProxy(mode: string) {
+//   const proxy = {
+//     development: {
+//       '/capital-api': 'http://127.0.0.1:3000/capital/',
+//     },
+//   };
+//   return proxy[mode] || {};
+// }
 
 // vite 配置
 export default ({ command, mode }: ConfigEnv): UserConfig => {
   const root = process.cwd();
   const env = loadEnv(mode, root);
   const viteEnv = wrapperEnv(env);
-  const { VITE_PUBLIC_PATH, VITE_DROP_CONSOLE, VITE_PORT, VITE_GLOB_PROD_MOCK, VITE_PROXY } = viteEnv;
-  const prodMock = VITE_GLOB_PROD_MOCK;
+  const { VITE_APP_TITLE, VITE_PUBLIC_PATH, VITE_DROP_CONSOLE, VITE_PORT, VITE_PROXY } = viteEnv;
   const isBuild = command === 'build';
+  // 输出文件夹
+  const OUTPUT_DIR = 'admin';
+  // 在生产环境中输入的配置文件的名称
+  const CONFIG_FILE_NAME = 'app.config.js';
+  // 处理接口参数默认路径
+  const getAppConfigSrc = () => {
+    const path = VITE_PUBLIC_PATH.endsWith('/') ? VITE_PUBLIC_PATH : `${VITE_PUBLIC_PATH}/`;
+    return `${path || '/'}${CONFIG_FILE_NAME}?v=${pkg.version}-${new Date().getTime()}`;
+  };
+
   return {
     base: VITE_PUBLIC_PATH,
     resolve: {
-      alias: [
-        {
-          find: /\/#\//,
-          replacement: pathResolve('types') + '/',
-        },
-        {
-          find: '@',
-          replacement: pathResolve('src') + '/',
-        },
-      ],
+      alias: {
+        '@': pathResolve('src'),
+        '/#/': pathResolve('types'),
+      },
       dedupe: ['vue'],
     },
-    plugins: createVitePlugins(viteEnv, isBuild, prodMock),
+    plugins: [
+      // @vitejs/plugin-vue
+      vue(),
+      // @vitejs/plugin-vue-jsx
+      vueJsx(),
+      // vite-plugin-html
+      html({
+        minify: isBuild, // 压缩index.html代码
+        inject: {
+          // 将数据注入ejs模板
+          injectData: {
+            title: VITE_APP_TITLE,
+          },
+          // 嵌入生成的app.config.js文件
+          tags: isBuild
+            ? [
+                {
+                  tag: 'script',
+                  attrs: {
+                    src: getAppConfigSrc(),
+                  },
+                },
+              ]
+            : [],
+        },
+      }),
+    ],
     // 定义全局常量替换方式。
     define: {
       __APP_INFO__: JSON.stringify(__APP_INFO__),
@@ -57,13 +127,6 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
       host: true,
       port: VITE_PORT,
       proxy: createProxy(VITE_PROXY),
-      // proxy: {
-      //     '/api': {
-      //         target: '',
-      //         changeOrigin: true,
-      //         rewrite: (path) => path.replace(/^\/api/, '/api/v1')
-      //     }
-      // }
     },
     // 依赖优化选项
     optimizeDeps: {
