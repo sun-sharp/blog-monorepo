a1020ebd9ec40ff8053f14998f88909e09428943
yangruirui <ncmuhengyue@163.com>
2022-02-14 23:54:58 +0800

    修改角色分页查询

--- src/modules/capital/role/dto/page-role.dto.ts
+++ src/modules/capital/role/dto/page-role.dto.ts
@@ -1,5 +1,4 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
 import { PaginateDto } from 'src/common/paginate/paginate.dto';
 
 export class PageRoleDto extends PaginateDto {
@@ -7,13 +6,11 @@ export class PageRoleDto extends PaginateDto {
     description: '角色名称',
     required: false,
   })
-  @IsString({ message: '角色名称必须是字符串' })
   readonly name: string;
 
   @ApiProperty({
     description: '角色标识',
     required: false,
   })
-  @IsString({ message: '角色标识必须是字符串' })
   readonly roleCode: string;
 }
