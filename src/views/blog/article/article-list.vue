8b1865aebc95fd303bf31c6e91fca83841fba1f7
yangruirui <ncmuhengyue@163.com>
2022-10-28 18:00:53 +0800

    文章管理

---
+++ src/views/blog/article/article-list.vue
@@ -0,0 +1,3 @@
+<template>
+  <n-card :bordered="false">列表</n-card>
+</template>
---
+++ src/views/blog/article/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <article-list v-show="showType === 'list'" />
+</template>
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import articleList from './article-list.vue';
+
+  const showType = ref('list');
+</script>
+<style lang="scss" scoped></style>
