42280cdecbfb512c82c0d38d7e08cbaa26e6cb81
yangruirui <ncmuhengyue@163.com>
2022-11-18 16:55:05 +0800

    修改 md-editor组件

---
+++ src/components/md-editor/md-editor-input.vue
@@ -0,0 +1,55 @@
+<template>
+  <md-editor-v3 v-model="text" :toolbars="toolbars" />
+</template>
+<script lang="ts">
+  import { defineComponent, ref } from 'vue';
+
+  const props = {
+    toolbars: {
+      type: Array,
+      default: [
+        'bold', // 加粗
+        'underline', // 加下划线
+        'italic', // 斜体
+        '-',
+        'title', // 标题
+        'strikeThrough', // 删除线
+        'sub', // 下标
+        'sup', // 上标
+        'quote', // 引用
+        'unorderedList', // 无序列表
+        'orderedList', // 有序列表
+        'task', // 任务列表
+        '-',
+        'codeRow', // 行内代码
+        'code', // 块级代码
+        'link', // 链接
+        'image', // 图片
+        'table', // 表格
+        'mermaid', // mermaid图
+        'katex', // katex公式
+        '-',
+        'revoke', // 后退
+        'next', // 前进
+        'save', // 保存
+        '=',
+        'pageFullscreen', // 浏览器全屏
+        'fullscreen', // 屏幕全屏
+        'preview', // 预览
+        'htmlPreview', // html代码预览
+        'catalog', // 目录
+        'github', // 源码地址
+      ],
+    }, // 开始数字
+  };
+
+  export default defineComponent({
+    name: 'CountTo',
+    props,
+    emits: ['onStarted', 'onFinished'],
+    setup() {
+      const text = ref('');
+      return { text };
+    },
+  });
+</script>
--- src/views/blog/article/article-add.vue
+++ src/views/blog/article/article-add.vue
@@ -4,65 +4,21 @@
       <span>md编辑器</span>
     </div>
 
-    <v-md-editor v-model="text" :left-toolbar="leftToolbar" height="500px" :disabled-menus="[]" @upload-image="handleUploadImage"></v-md-editor>
+    <md-editor-input />
   </div>
 </template>
 
 <script lang="ts">
-  const text = `
-<h2 align="center">Markdown Editor built on Vue</h2>
-
-::: tip 我可以支持公式
-  $$\\sum_{i=1}^n a_i=0$$
-:::
-
-::: warning 我还支持表情
-  :grinning:
-:::
-
-::: danger 还有任务列表
-  - [ ] Task
-:::
-
-::: details
-  这是一个详情块，在 IE / Edge 中不生效
-:::
-
-::: tip 流程图 (请将index.html文件中的引入的外部流程图js取消注释，并且将md-editor.ts流程图注释部分取消)
-\`\`\`mermaid
-graph LR
-A --- B
-B-->C[fa:fa-ban forbidden]
-B-->D(fa:fa-spinner);
-\`\`\`
-:::
-
-## 引用
-
-- [v-md-editor](https://ckang1229.gitee.io/vue-markdown-editor/zh)
-  `;
-
   import { defineComponent } from 'vue';
+  import MdEditorInput from '@/components/md-editor/md-editor-input.vue';
+
   export default defineComponent({
     name: 'ArticleAdd',
+    components: {
+      MdEditorInput,
+    },
     setup() {
-      const handleUploadImage = (event: Event, insertImage: Function, files: File[]) => {
-        // 拿到 files 之后上传到文件服务器，然后向编辑框中插入对应的内容
-        console.log(event, files);
-
-        // 此处只做示例
-        insertImage({
-          url: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1269952892,3525182336&fm=26&gp=0.jpg',
-          desc: '七龙珠',
-          // width: 'auto',
-          // height: 'auto',
-        });
-      };
-      return {
-        text,
-        leftToolbar: 'undo redo clear | emoji todo-list | h bold italic strikethrough quote | ul ol table hr | link image code | save',
-        handleUploadImage,
-      };
+      return {};
     },
   });
 </script>
