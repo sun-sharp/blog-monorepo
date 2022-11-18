72efb4c6de664eb937fe388225a820a4e7532f6f
yangruirui <ncmuhengyue@163.com>
2022-11-18 10:28:19 +0800

    修改切换页面问题

--- src/views/blog/article/index.vue
+++ src/views/blog/article/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <article-list v-show="showType === 'list'" />
-  <!-- <article-add v-if="showType === 'add'" /> -->
+  <n-card :bordered="false">
+    <article-list v-show="showType === 'list'" />
+    <article-add v-if="showType === 'add'" />
+  </n-card>
 </template>
 <script lang="ts" setup>
   import { ref } from 'vue';
   import articleList from './article-list.vue';
-  // import articleAdd from './article-add.vue';
+  import articleAdd from './article-add.vue';
 
-  const showType = ref('list');
+  const showType = ref('add');
 </script>
 <style lang="scss" scoped></style>
