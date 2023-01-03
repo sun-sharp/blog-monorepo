5495b6ffb048a7c2fa6f4b1a0b82b82279644acb
yangruirui <ncmuhengyue@163.com>
2023-01-03 18:34:08 +0800

    添加顶部字段

--- src/modules/capital/configuration/dto/create-configuration.dto.ts
+++ src/modules/capital/configuration/dto/create-configuration.dto.ts
@@ -1,6 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsNotEmpty } from 'class-validator';
 
+class HeaderSettingDto {
+  @ApiProperty({
+    description: '固定顶部',
+    example: true,
+  })
+  @IsBoolean({ message: '显示重载按钮' })
+  fixed: boolean;
+
+  @ApiProperty({
+    description: '显示重载按钮',
+    example: true,
+  })
+  @IsBoolean({ message: '显示重载按钮' })
+  isReload: boolean;
+}
+
 export class CreateConfigurationDto {
   @ApiProperty({
     description: '深色主题',
@@ -15,4 +31,9 @@ export class CreateConfigurationDto {
   })
   @IsNotEmpty({ message: '系统主题色不能为空' })
   appTheme: string;
+
+  @ApiProperty({
+    description: '顶部',
+  })
+  headerSetting: HeaderSettingDto;
 }
--- src/schemas/capital/configuration.schema.ts
+++ src/schemas/capital/configuration.schema.ts
@@ -2,6 +2,21 @@ import { SchemaFactory, Prop, Schema } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 /**
+ * @description 顶部字段
+ * @export
+ * @class HeaderSetting
+ */
+export class HeaderSetting {
+  //固定顶部
+  @Prop()
+  fixed: true;
+
+  //显示重载按钮
+  @Prop()
+  isReload: true;
+}
+
+/**
  * @description 设置数据字段
  * @export
  * @class Configuration
@@ -19,6 +34,9 @@ export class Configuration extends Document {
 
   @Prop()
   appTheme: string; // 系统主题色
+
+  @Prop()
+  headerSetting: HeaderSetting; // 顶部
 }
 
 export const ConfigurationSchema = SchemaFactory.createForClass(Configuration);
