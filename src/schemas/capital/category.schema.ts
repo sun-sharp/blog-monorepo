ea5b798a94d2ddf297e9f19d7d442ed1484a2ae9
yangruirui <ncmuhengyue@163.com>
2023-04-13 16:30:02 +0800

    添加-分类数据字段

---
+++ src/schemas/capital/category.schema.ts
@@ -0,0 +1,24 @@
+import { SchemaFactory, Prop, Schema } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+/**
+ * @description 分类数据字段
+ * @export
+ * @class category
+ * @extends {Document}
+ */
+@Schema({
+  versionKey: false,
+})
+export class Category extends Document {
+  @Prop()
+  type: string; // 分类类型
+
+  @Prop()
+  value: number; // 分类标识
+
+  @Prop()
+  name: string; // 分类名称
+}
+
+export const CategorySchema = SchemaFactory.createForClass(Category);
