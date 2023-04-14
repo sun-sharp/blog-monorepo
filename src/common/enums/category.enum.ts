43a405273fde9be72dc17e0fc6dc06819abc1794
yangruirui <ncmuhengyue@163.com>
2023-04-14 17:54:50 +0800

    修改分类类型

--- src/common/enums/category.enum.ts
+++ src/common/enums/category.enum.ts
@@ -1,4 +1,7 @@
 // 分类类型
 export enum categoryTypeEnum {
   moneyBillType = 'money_bill_type', // 金额账单类型
+  moneyBankType = 'money_bank_type', // 金额银行类型
+  moneyBillMethod = 'money_bill_method', // 金额账单方式
+  capitalMenuType = 'capital_menu_type', // 金额账单类型
 }
