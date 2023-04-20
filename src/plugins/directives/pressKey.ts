302a70a56d2e3fe6f882dfb9d6ca633ea55f355f
yangruirui <ncmuhengyue@163.com>
2023-04-20 17:29:46 +0800

    修改监听键盘指令

--- src/plugins/directives/pressKey.ts
+++ src/plugins/directives/pressKey.ts
@@ -18,20 +18,12 @@ interface keys {
 }
 const keys: keys = {};
 
-// 判断类型
-const ifType = (el: Element): boolean => {
-  return el.tagName == 'INPUT' || el.tagName == 'TEXTAREA';
-};
-
 // 等待时间
 const wait = 100;
 
 // 监听键盘使用
 export const pressKey: ObjectDirective = {
-  mounted(el, bind) {
-    // 判断是否是 input 或者 textarea 由于 el-input是一个div元素且它的下级才是input 故此获取children
-    const inputNode = ifType(el) ? el : el.children.length && ifType(el.children[0]) ? el.children[0] : undefined;
-
+  mounted(_el, bind) {
     if (!bind.arg) {
       console.error('请绑定需要触发的键，例如v-press-key:s，v-press-key:s.alt');
       return;
@@ -39,29 +31,23 @@ export const pressKey: ObjectDirective = {
 
     // 获取键盘组合键盘值
     const modifiersArr = Object.keys(bind.modifiers).filter((f) => bind.modifiers[f]);
-    // 获取id
-    const modifiersIds = modifiersArr.filter((f) => !['ctrl', 'alt', 'shift'].includes(f));
-    const id = modifiersIds.length ? modifiersIds[0] : '';
 
     // 获取按键值
-    let keypad = bind.arg;
+    let k = bind.arg;
     if (modifiersArr.includes('ctrl')) {
-      keypad = keypad + '.ctrl';
+      k = k + '.ctrl';
     }
     if (modifiersArr.includes('alt')) {
-      keypad = keypad + '.alt';
+      k = k + '.alt';
     }
     if (modifiersArr.includes('shift')) {
-      keypad = keypad + '.shift';
+      k = k + '.shift';
     }
 
-    if (Object.keys(keys).filter((item) => item == keypad).length) {
-      console.error('绑定的按键 ' + keypad + ' 与已有的重名');
+    if (Object.keys(keys).filter((item) => item == k).length) {
+      console.error('绑定的按键 ' + k + ' 与已有的重名');
       return;
     }
-
-    // 获取对象键值
-    const k = inputNode !== undefined ? keypad + '-' + inputNode.tagName + (id ? '-' + id : '') : keypad;
     // 储存数据
     keys[k] = {
       arg: bind.arg,
@@ -79,21 +65,6 @@ export const pressKey: ObjectDirective = {
     if (modifiersArr.includes('shift')) {
       keys[k].shift = true;
     }
-
-    // // 绑定在input上时
-    // if (inputNode !== undefined) {
-    //   inputNode.onkeydown = function keydown(event: KeyboardEvent) {
-    //     // 获取匹配项
-    //     const match = Object.keys(keys).filter((item) => {
-    //       const key = item.split('-')[0];
-    //       return event.key.toUpperCase() == key || event.key.toLowerCase() == key || event.key == key;
-    //     });
-
-    //     useThrottleFn(match.length && keys[match[0].split('-')[0] + '-' + inputNode.tagName + (id ? '-' + id : '')].funVal(), wait);
-    //   };
-
-    //   return;
-    // }
 
     window.onkeydown = function keydown(event: KeyboardEvent) {
       console.log(event, 'event');
