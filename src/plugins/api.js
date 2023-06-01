663b7cd717fca16a100c034a23b4923b042a6cce
sun-sharp <ncmuhengyue@163.com>
2023-06-01 16:22:47 +0800

    首页预览问题

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,9 +1,9 @@
 // 后台接口api的链接
 export const API = {
-  ARTICLE_FIND: '/blog/article/find', // 条件查询文章列表
-  ARTICLE_DETAILS: '/blog/article/details', // 查询文章详情
-  BANNER_FIND: '/blog/banner/find', // 查询banner
-  CATEGORY_FIND_ALL: '/blog/category/findAll' // 获取全部分类类型
+  ARTICLE_FIND: '/blog-api/article/find_page', // 条件查询文章列表
+  ARTICLE_DETAILS: '/blog-api/article/details', // 查询文章详情
+  BANNER_FIND: '/blog-api/banner/find', // 查询banner
+  CATEGORY_FIND_ALL: '/blog-api/article-category/all' // 获取全部分类类型
 };
 
 export default API;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,16 +4,16 @@ let target = '';
 if ( process.env.BABEL_ENV === "development") {
   target = "http://localhost:3000";
 } else {
-  target = "https://www.yangruirui.top";
+  target = "https://api.yangruirui.top";
 }
 console.log(process.env.BABEL_ENV, "运行环境", target);
 module.exports = function(app) {
   app.use(
-    proxy('/blog', {
+    proxy('/blog-api', {
       target,
       changeOrigin: true,
       pathRewrite: {
-        '^/blog': '/blog',
+        '^/blog-api': '/blog',
       },
     }),
   );
