63cf5e7932a5dad68747cdeca88094f59f7168cc
sun-sharp <ncmuhengyue@163.com>
2023-06-16 18:04:52 +0800

    修改滚动样式

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -6,9 +6,27 @@ import { Button } from 'antd';
 const Home: React.FC = () => {
   const [count, setCount] = useState(0);
 
+  // return (
+  //   <>
+  // <div>首页</div>
+  //     <h1>
+  //       <a href="https://react.dev" target="_blank">
+  //         <img src={reactLogo} className="logo react" alt="React logo" />
+  //       </a>
+  //     </h1>
+  //     <div className="card">
+  //       <Button type="primary" onClick={() => setCount((count) => count + 1)}>
+  //         count is {count}
+  //       </Button>
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to test HMR
+  //       </p>
+  //     </div>
+  //     <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+  //   </>
+  // );
   return (
-    <>
-      <div>首页</div>
+    <div className="home">
       <h1>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -23,7 +41,7 @@ const Home: React.FC = () => {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   );
 };
 
