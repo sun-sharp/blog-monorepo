d57c8b1557695e1c0a99bb93c7f706c9c20e31d3
powerfulYangRR <ncmuhengyue@163.com>
2023-06-21 20:38:45 +0800

    修改文章详情

--- src/components/common/ArticleItem/index.tsx
+++ src/components/common/ArticleItem/index.tsx
@@ -1,9 +1,10 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './index.scss';
 import { IArticleItemProps } from '/#/components/common';
 import { IArticleItem } from '/#/views/home';
 
 const ArticleItem: React.FC<IArticleItemProps> = ({ data }) => {
+  const navigate = useNavigate();
   return (
     <ul className="article-cont">
       {data.map((item: IArticleItem) => {
@@ -11,31 +12,33 @@ const ArticleItem: React.FC<IArticleItemProps> = ({ data }) => {
         return (
           <li key={item.articleId} className="article-item">
             <div className="item-cont">
-              {/* {item.image ? (
-            <div className="item-cont--img">
-              <Link to={linkTo} title={item.title}>
-                <img src={item.image} alt={item.title} />
-              </Link>
-            </div>
-          ) : (
-            ''
-          )} */}
-              <div>
-                <Link to={linkTo}>
-                  <p className="item-cont--title">{item.title}</p>
-                </Link>
-                <p className="item-cont--info">{item.brief}</p>
+              <div
+                className="item-cont--title"
+                onClick={() => {
+                  navigate(linkTo);
+                }}>
+                {item.title}
               </div>
-            </div>
-            <div className="item-author">
-              <span className="lm">
-                <i className="icon-tag2" />
-                {item.categoryName}
-              </span>
-              <span className="time">
-                <i className="icon-time1" />
-                {item.createTime}
-              </span>
+              <div className="item-author">
+                <span className="lm">
+                  {/* <i className="icon-tag2" /> */}
+                  {item.categoryName}
+                </span>
+                <span className="time">
+                  {/* <i className="icon-time1" /> */}
+                  {item.createTime}
+                </span>
+              </div>
+              <div className="item-cont--info">{item.brief}</div>
+              <div className="item-cont--btn">
+                <span
+                  className="more"
+                  onClick={() => {
+                    navigate(linkTo);
+                  }}>
+                  阅读原文
+                </span>
+              </div>
             </div>
           </li>
         );
