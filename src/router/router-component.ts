ad15ac6e58100357c348cc363dcae02be2c0d4b7
sun-sharp <ncmuhengyue@163.com>
2023-09-15 17:26:11 +0800

    修改页面命名问题

--- src/router/router-component.ts
+++ src/router/router-component.ts
@@ -1,23 +1,23 @@
-// // 根目录
+// 根目录
 export const Layout = () => import('@/layout/LayoutIndex.vue');
 
 // // 全局
 // export const ParentLayout = () => import('@/layout/layout-parent.vue');
 
-// // 首页
-export const HomeComponent = () => import('@/views/home/HomeIndex.vue');
+// 首页
+export const HomeComponent = () => import('@/views/home/index.vue');
 
 // // iframe嵌入
 // export const IframeComponent = () => import('@/views/iframe.vue');
 
 // 404页面
-export const Error404Component = () => import('@/views/Error404.vue');
+export const Error404Component = () => import('@/views/error-404.vue');
 
 // // 重定向页面
 // export const RedirectComponent = () => import('@/views/redirect.vue');
 
 // 登录页面
-export const LoginComponent = () => import('@/views/common/Login.vue');
+export const LoginComponent = () => import('@/views/common/login.vue');
 
 // 基本信息设置页面
 // export const SettingAccountComponent = () => import('@/views/common/setting-account.vue');
