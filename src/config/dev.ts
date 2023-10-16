0a815c248d4e56b0b4e2f9615bcde494af810083
sun-sharp <ncmuhengyue@163.com>
2023-10-16 10:23:37 +0800

    修改部署环境

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -1,16 +1,16 @@
 import { GlobalEnvConfig } from 'types/config';
 
 const glob: Readonly<GlobalEnvConfig> = {
-  port: 3300,
+  port: 3000,
   serverIp: '120.79.162.189',
   databasePort: 5606,
   mongodbAccount: 'yrr',
   mongodbPassword: 'AlyYrrAdmin123',
   mongodbQuery: '?authSource=admin',
-  fileAccessPath: 'api_dev_file',
-  staticDirPosition: '../',
-  staticDirName: 'api_dev_static_dir',
-  imageRefixName: 'sharp_dev_network_',
+  fileAccessPath: 'api_file',
+  staticDirPosition: './',
+  staticDirName: 'api_static_dir',
+  imageRefixName: 'sharp_local_',
   capitalDatabaseName: 'dev_capital',
   blogDatabaseName: 'dev_blog',
 };
