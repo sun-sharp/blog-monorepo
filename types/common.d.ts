54cb809f40e7d918aa3f2e1d8f83983ee12812d5
sun-sharp <ncmuhengyue@163.com>
2023-10-17 18:30:24 +0800

    修改 用户的配置信息 接口

--- types/common.d.ts
+++ types/common.d.ts
@@ -1,8 +1,8 @@
 /**
  * @description 返回报文格式
  */
-export interface IResponse {
+export interface IResponse<T = any> {
   code: number; // 0 表示成功
-  result?: any;
+  result?: T;
   message: string;
 }
