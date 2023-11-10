c96669b164c0dd6726daa0ae0a320d13a71af456
sun-sharp <ncmuhengyue@163.com>
2023-11-10 18:22:50 +0800

    修改导入支付宝，微信，银行账单方式

--- src/common/utils/money.ts
+++ src/common/utils/money.ts
@@ -77,18 +77,17 @@ export const weChatExcelTargetHandler = (target: any) => {
 export const aliPayExcelTargetHandler = (target: any) => {
   const oldTarget = JSON.parse(JSON.stringify(target));
   const tradeType = oldTarget.tradeType;
-  const paymentMethod = oldTarget.paymentMethod;
-  const transactionClassification = oldTarget.transactionClassification;
+  const paymentMethod = oldTarget.paymentMethod || '';
   const tradeOtherPerson = oldTarget.tradeOtherPerson;
   const productDescription = oldTarget.productDescription;
   const tradeStatus = oldTarget.tradeStatus;
-  if (['余额', '账户余额'].includes(paymentMethod) || '余额&'.indexOf(paymentMethod) !== -1) {
+  if (['余额', '账户余额'].includes(paymentMethod) || paymentMethod.indexOf('余额&') !== -1) {
     target.billMethod = billMethodEnum.aliPayBalance;
   } else if (['余额宝'].includes(paymentMethod) || ['余额宝-笔笔攒-单笔攒入', '余额宝-单次转入'].includes(productDescription)) {
     target.billMethod = billMethodEnum.aliPayBalanceBaby;
   } else if (['花呗', '花呗&红包'].includes(paymentMethod)) {
     target.billMethod = billMethodEnum.aliPayHuaBei;
-  } else if (['中国工商银行储蓄卡(3413)'].includes(paymentMethod)) {
+  } else if (['中国工商银行储蓄卡(3413)', '工商银行储蓄卡(3413)'].includes(paymentMethod)) {
     target.billMethod = billMethodEnum.business;
   } else if (['单车骑行卡抵扣', '医保支付(不含自费)'].includes(productDescription)) {
     target.billMethod = billMethodEnum.other;
@@ -97,9 +96,10 @@ export const aliPayExcelTargetHandler = (target: any) => {
   if (tradeStatus === '交易关闭') {
     target.billType = billTypeEnum.invalid;
   } else if (
-    transactionClassification === '交通出行' &&
-    tradeOtherPerson === '成都金控数据服务有限公司' &&
-    ['天府通扫码乘车', '天府通APP乘车'].includes(productDescription)
+    (tradeType === '交通出行' &&
+      ['成都金控数据服务有限公司', '成都天府通数字科技有限公司'].includes(tradeOtherPerson) &&
+      ['天府通扫码乘车', '天府通APP乘车'].includes(productDescription)) ||
+    ['地铁', '一应黔行', '重庆轨道交通'].find((f) => tradeOtherPerson.indexOf(f) !== -1)
   ) {
     target.billType = billTypeEnum.trafficSubway;
   } else if (['余额宝-笔笔攒-单笔攒入', '余额宝-单次转入'].includes(productDescription)) {
@@ -116,8 +116,6 @@ export const aliPayExcelTargetHandler = (target: any) => {
     target.billType = billTypeEnum.trafficBicycle;
   } else if (['滴滴出行', '高德打车'].includes(tradeOtherPerson)) {
     target.billType = billTypeEnum.trafficTaxi;
-  } else if (['成都金控数据服务有限公司', '地铁', '一应黔行'].find((f) => tradeOtherPerson.indexOf(f) !== -1)) {
-    target.billType = billTypeEnum.trafficSubway;
   } else if (['便利店', '超市', '红旗连锁'].find((f) => tradeOtherPerson.indexOf(f) !== -1)) {
     target.billType = billTypeEnum.consumptionSupermarket;
   } else if (['成都空港公共交通有限公司'].includes(tradeOtherPerson)) {
