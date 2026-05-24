773dbe80dea115397b1dcebca1e01e225ec04741
powerfulYangRR <ncmuhengyue@163.com>
2026-05-24 17:26:54 +0800

    删除 admin_mobile1

--- packages/admin-mobile1/src/constant/index.ts
+++
@@ -1,6 +0,0 @@
-export * from '@shared/constants/storage-name';
-export * from '@shared/constants/http-enum';
-export * from '@shared/constants/api-type';
-export * from '@shared/constants/page-const';
-export * from '@shared/constants/table-const';
-export * from './env-config';
--- packages/admin-mobile1/src/store/modules/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './user';
-export * from './lock-screen';
-export * from './api-type';
