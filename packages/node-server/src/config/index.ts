49471b842bbe78bac7326b95cf5fca8c2575a4a6
powerfulYangRR <ncmuhengyue@163.com>
2026-08-05 22:49:54 +0800

    重构配置管理，简化环境变量处理并更新.gitignore以包含环境文件

--- packages/node-server/src/config/index.ts
+++ packages/node-server/src/config/index.ts
@@ -1,23 +1,39 @@
 import { GlobalEnvConfig } from '/#/api/config';
-import devConfig from './dev';
-import prodConfig from './prod';
 import * as dotenv from 'dotenv';
 import { resolve } from 'node:path';
 
 dotenv.config({ path: resolve(process.cwd(), '.env') });
 
-const configs = {
-  dev: devConfig,
-  prod: prodConfig,
-};
+const {
+  PORT,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  BLOG_DATABASE_NAME,
+  CAPITAL_DATABASE_NAME,
+  IMAGE_REFIX_NAME,
+  STATIC_DIR_NAME,
+  STATIC_DIR_POSITION,
+  FILE_ACCESS_PATH,
+  SERVER_IP,
+  DATABASE_PORT,
+  MONGODB_ACCOUNT,
+  MONGODB_PASSWORD,
+  MONGODB_QUERY,
+} = process.env;
 
-const { RUNNING_ENV } = process.env;
-
-console.log(RUNNING_ENV, 'RUNNING_ENV');
-
-let env = 'prod';
-if (RUNNING_ENV === 'dev') {
-  env = 'dev';
-}
-
-export const useCustomConfig = (): GlobalEnvConfig => configs[env];
+export const useCustomConfig = (): GlobalEnvConfig => ({
+  port: Number(PORT) || 3000,
+  jwtSecret: JWT_SECRET || 'xxx',
+  jwtExpiresIn: JWT_EXPIRES_IN || '1d',
+  blogDatabaseName: BLOG_DATABASE_NAME || 'blog',
+  capitalDatabaseName: CAPITAL_DATABASE_NAME || 'capital',
+  imageRefixName: IMAGE_REFIX_NAME || 'sharp_',
+  staticDirName: STATIC_DIR_NAME || 'api_static_dir',
+  staticDirPosition: STATIC_DIR_POSITION || '../',
+  fileAccessPath: FILE_ACCESS_PATH || 'api_file',
+  serverIp: SERVER_IP || '0.0.0.0',
+  databasePort: Number(DATABASE_PORT) || 1111,
+  mongodbAccount: MONGODB_ACCOUNT || 'xxxx',
+  mongodbPassword: MONGODB_PASSWORD || 'xxxx',
+  mongodbQuery: MONGODB_QUERY || '?xx=xx',
+});
