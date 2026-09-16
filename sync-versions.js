a35b0bc1017439b927dc6f8c538b07d29d7a56a9
powerfulYangRR <ncmuhengyue@163.com>
2026-09-17 00:36:05 +0800

    添加 版本标签 说明 和修改版本号

--- sync-versions.js
+++ sync-versions.js
@@ -4,8 +4,8 @@ const path = require('path');
 // 先安装：npm install json5
 const JSON5 = require('json5');
 
-const newVersion = '4.5.9';
-const newVersionCode = 4509;
+const newVersion = '4.5.10';
+const newVersionCode = 4510;
 
 // ---------- package.json 列表 ----------
 const packageFiles = [
