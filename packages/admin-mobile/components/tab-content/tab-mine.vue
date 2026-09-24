3ec2adc4a15b93f6a8bcbacb141a40f33992e3c5
powerfulYangRR <ncmuhengyue@163.com>
2026-09-25 01:42:14 +0800

    修改 颜色

--- packages/admin-mobile/components/tab-content/tab-mine.vue
+++ packages/admin-mobile/components/tab-content/tab-mine.vue
@@ -7,7 +7,7 @@
             <u-icon name="setting" size="40" :color="isDark ? '#fff' : '#333'" />
           </view>
           <view class="mine-tool-btn" @click="toggleTheme">
-            <u-icon :name="isDark ? 'yueliang' : 'taiyang'" custom-prefix="sharp-icon" size="40" :color="isDark ? '#fff' : '#333'" />
+            <u-icon :name="isDark ? 'yueliang' : 'taiyang'" custom-prefix="sharp-icon" size="40" :color="isDark ? '#fff' : '#F9C626'" />
           </view>
         </view>
       </view>
